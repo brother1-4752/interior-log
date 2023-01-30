60258d0d4e7e8e9061ffbf2dfe7144c727066afe
brother1-4752 <60454376+brother1-4752@users.noreply.github.com>
2023-01-30 20:26:45 +0900

    fix todo.js

--- js/journal.js
+++ js/journal.js
@@ -2,11 +2,16 @@ const plusBtn = document.querySelector(".plus");
 const boughtBtn = document.querySelector(".bought");
 const pendingBox = document.querySelector(".pending__container");
 const boughtBox = document.querySelector(".bought__container");
-const pendingList = document.querySelector(".pending--list");
-const boughtList = document.querySelector(".bought--list");
 
 const journalForm = document.querySelector(".journal__header--form");
 const journalInput = document.querySelector(".journal__header--input");
+const pendingList = document.querySelector(".pending--list");
+const boughtList = document.querySelector(".bought--list");
+
+let PENDING_TODOS = [];
+let BOUGHT_TODOS = [];
+const PENDING_KEY = "pending";
+const BOUGHT_KEY = "BOUGHT";
 
 function showContainer(e) {
   const currentBox =
@@ -21,53 +26,116 @@ function showContainer(e) {
   }
 }
 
-function submitJournalForm(e) {
-  function clickDoneBtn(e) {
-    function clickBackBtn(e) {
-      const thingToBeBack = e.target.parentElement.parentElement;
-      pendingList.appendChild(thingToBeBack);
-      e.target.parentElement.classList.add(HIDE_CLASSNAME);
-      e.target.parentElement.previousElementSibling.classList.remove(
-        HIDE_CLASSNAME
-      );
-    }
+function saveBoughtToDos() {
+  localStorage.setItem(BOUGHT_KEY, JSON.stringify(BOUGHT_TODOS));
+}
 
-    const thingToBeDone = e.target.parentElement.parentElement;
-    boughtList.appendChild(thingToBeDone);
-    e.target.parentElement.classList.add(HIDE_CLASSNAME);
+function savePendingToDos() {
+  localStorage.setItem(PENDING_KEY, JSON.stringify(PENDING_TODOS));
+}
 
-    const backBtn = document.createElement("div");
-    thingToBeDone.appendChild(backBtn);
-    backBtn.innerHTML = `<i class="fa-solid fa-left-long"></i>`;
+// function clickBackBtn(e) {
+//   const thingToBeBack = e.target.parentElement.parentElement;
+//   pendingList.appendChild(thingToBeBack);
+//   e.target.parentElement.classList.add(HIDE_CLASSNAME);
+//   e.target.parentElement.previousElementSibling.classList.remove(
+//     HIDE_CLASSNAME
+//   );
+// }
 
-    backBtn.addEventListener("click", clickBackBtn);
-  }
+function paintBoughtTodos(obj) {
+  const deleteBtn = document.createElement("div");
+  const backBtn = document.createElement("div");
+  const boughtItem = document.createElement("li");
+  const boughtSpan = document.createElement("span");
 
-  function clickDeleteBtn(e) {
-    const thingToDelete = e.target.parentElement.parentElement;
-    thingToDelete.remove();
-  }
+  boughtItem.id = obj.id;
 
-  e.preventDefault();
-  const todo = journalInput.value;
+  boughtItem.appendChild(boughtSpan);
+  boughtItem.appendChild(deleteBtn);
+  boughtItem.appendChild(backBtn);
+  boughtSpan.innerText = obj.text;
+  deleteBtn.innerHTML = `<i class="fa-solid fa-trash"></i>`;
+  deleteBtn.addEventListener("click", clickDeleteBtn);
+  backBtn.innerHTML = `<i class="fa-solid fa-left-long"></i>`;
+  // backBtn.addEventListener("click", clickbackBtn);
+  boughtList.appendChild(boughtItem);
+}
+
+function clickDoneBtn(e) {
+  const thingToBeDone = e.target.parentElement.parentElement;
+  const todo = thingToBeDone.innerText;
+  const doneTodoObj = {
+    text: todo,
+    id: thingToBeDone.id,
+  };
+  BOUGHT_TODOS.push(doneTodoObj);
+  paintBoughtTodos(doneTodoObj);
+
+  PENDING_TODOS = PENDING_TODOS.filter(
+    (t) => t.id !== parseInt(thingToBeDone.id)
+  );
+  thingToBeDone.remove();
+  saveBoughtToDos();
+  savePendingToDos();
+}
+
+function clickDeleteBtn(e) {
+  const thingToDelete = e.target.parentElement.parentElement;
+  console.dir(thingToDelete);
+  PENDING_TODOS = PENDING_TODOS.filter(
+    (todo) => todo.id !== parseInt(thingToDelete.id)
+  );
+
+  BOUGHT_TODOS = BOUGHT_TODOS.filter(
+    (todo) => todo.id !== parseInt(thingToDelete.id)
+  );
+  savePendingToDos();
+  saveBoughtToDos();
+  thingToDelete.remove();
+}
+
+function paintPendingTodos(newObj) {
   const deleteBtn = document.createElement("div");
   const doneBtn = document.createElement("div");
   const pendingItem = document.createElement("li");
   const pendingSpan = document.createElement("span");
-  pendingList.appendChild(pendingItem);
+
+  pendingItem.id = newObj.id;
+
   pendingItem.appendChild(pendingSpan);
   pendingItem.appendChild(deleteBtn);
   pendingItem.appendChild(doneBtn);
-  pendingSpan.innerHTML = `${todo}`;
+  pendingSpan.innerText = newObj.text;
   deleteBtn.innerHTML = `<i class="fa-solid fa-trash"></i>`;
-  doneBtn.innerHTML = `<i class="fa-solid fa-circle-check"></i>`;
-  localStorage.setItem("THING_TO_BUY", todo);
-  journalInput.value = "";
-
   deleteBtn.addEventListener("click", clickDeleteBtn);
+  doneBtn.innerHTML = `<i class="fa-solid fa-circle-check"></i>`;
   doneBtn.addEventListener("click", clickDoneBtn);
+  pendingList.appendChild(pendingItem);
+}
+
+function submitJournalForm(e) {
+  e.preventDefault();
+  const todo = journalInput.value;
+  journalInput.value = "";
+  const newTodoObj = {
+    text: todo,
+    id: Date.now(),
+  };
+
+  PENDING_TODOS.push(newTodoObj);
+  paintPendingTodos(newTodoObj);
+  savePendingToDos();
 }
 
 plusBtn.addEventListener("click", showContainer);
 boughtBtn.addEventListener("click", showContainer);
 journalForm.addEventListener("submit", submitJournalForm);
+
+const savedPendingToDos = localStorage.getItem(PENDING_KEY);
+
+if (savedPendingToDos !== null) {
+  const parsedPendingToDos = JSON.parse(savedPendingToDos);
+  PENDING_TODOS = parsedPendingToDos;
+  parsedPendingToDos.forEach(paintPendingTodos);
+}
