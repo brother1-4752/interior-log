1ac2293a1d4c4b36570f0c564389099fc0e37b46
brother1-4752 <60454376+brother1-4752@users.noreply.github.com>
2023-01-31 18:40:35 +0900

    add maincontent-imgs and links until deco/plant

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,7 @@
 const menus = document.querySelectorAll(".category-item");
 const pages = document.querySelectorAll(".contents");
+const sidebarMenus = document.querySelectorAll(".category-row > span");
+
 let covertedPages = Array.from(pages);
 
 function switchScreen(e) {
@@ -21,6 +23,28 @@ function switchScreen(e) {
   });
 }
 
+function switchScreenBySidebarMenus(e) {
+  const title = e.target.innerText;
+  covertedPages.forEach((page) => {
+    if (
+      page.children[0].innerText === title &&
+      page.classList.contains("hide")
+    ) {
+      let removeCurrentPage = covertedPages.filter((page) =>
+        page.classList.contains("show")
+      );
+      removeCurrentPage[0].classList.remove("show");
+      removeCurrentPage[0].classList.add("hide");
+      page.classList.remove("hide");
+      page.classList.add("show");
+    }
+  });
+}
+
 menus.forEach((menu) => {
   menu.addEventListener("click", switchScreen);
+});
+
+sidebarMenus.forEach((s_menu) => {
+  s_menu.addEventListener("click", switchScreenBySidebarMenus);
 });
